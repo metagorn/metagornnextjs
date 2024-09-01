75ea739f374975dfab4e612f07cf3870bb9fc702
metagorn <119285215+metagorn@users.noreply.github.com>
2024-09-01 23:45:31 +0700

    .

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+export interface resPokemons{
+    name: string;
+    url: string;
+}
+
 export default async function page() {
     const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
     const json = await res.json();
-    console.log(json);
+    const pokemons: resPokemons[] = json.results;
   return (
     <div>page</div>
   )
